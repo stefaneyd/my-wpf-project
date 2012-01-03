4b588576a3739332591610097443b6e39b487059
stefan.eydal <stefan.eydal@gmail.com>
2012-01-03 17:31:52 +0000

    Project uploaded to google code

---
+++ WPF Project/BindingExample/BindingExample/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BindingExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person src = new Person { Name = "Stefán", Age = 39 };
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = src;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string message = src.Name + " is " + src.Age;
+            MessageBox.Show(message);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            src.Age += 1;
+
+            //ageText.GetBindingExpression(TextBox.TextProperty).UpdateTarget();
+        }
+    }
+}
---
+++ WPF Project/DataTemplate/DataTemplate/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataTemplate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person src = new Person { Name = "Stefán", Age = 39 };
+        List<Person> people = new List<Person>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            people.Add(src);
+            people.Add(new Person { Name = "Stefanía", Age = 36 });
+            people.Add(new Person { Name = "Pétur", Age = 17 });
+            this.DataContext = people;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string message = src.Name + " is " + src.Age;
+            MessageBox.Show(message);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            src.Age += 1;
+
+            //ageText.GetBindingExpression(TextBox.TextProperty).UpdateTarget();
+        }
+    }
+}
